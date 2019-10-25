5e1bf84e8db22fca69ec41af229372c0bec7d755
Ingo <roessner@infai.org>
2019-10-25 10:13:28 +0200

    test for device creation on publish

---
+++ test/iot_setup_test.go
@@ -0,0 +1,102 @@
+package test
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"github.com/SENERGY-Platform/mqtt-platform-connector/lib"
+	"github.com/SENERGY-Platform/mqtt-platform-connector/test/helper"
+	"github.com/SENERGY-Platform/platform-connector-lib/model"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"testing"
+	"time"
+)
+
+func createTestProtocol(t *testing.T, config lib.Config) model.Protocol {
+	protocol := model.Protocol{}
+	err := helper.AdminJwt.PostJSON(config.DeviceManagerUrl+"/protocols", model.Protocol{
+		Name:             config.Protocol,
+		Handler:          config.Protocol,
+		ProtocolSegments: []model.ProtocolSegment{{Name: "metrics"}},
+	}, &protocol)
+	if err != nil {
+		t.Fatal(err)
+	}
+	return protocol
+}
+
+func createTestDeviceType(t *testing.T, config lib.Config, protocol model.Protocol, serviceLocalId string) (result model.DeviceType) {
+	err := helper.AdminJwt.PostJSON(config.DeviceManagerUrl+"/device-types", model.DeviceType{
+		Name: "testDeviceType",
+		Services: []model.Service{
+			{
+				Name:        serviceLocalId,
+				LocalId:     serviceLocalId,
+				Description: serviceLocalId,
+				ProtocolId:  protocol.Id,
+				Outputs: []model.Content{
+					{
+						ProtocolSegmentId: protocol.ProtocolSegments[0].Id,
+						Serialization:     "json",
+						ContentVariable: model.ContentVariable{
+							Name: "metrics",
+							Type: model.Structure,
+							SubContentVariables: []model.ContentVariable{
+								{
+									Name: "level",
+									Type: model.Integer,
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}, &result)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if result.Id == "" {
+		t.Fatal("unexpected result", result)
+	}
+	return
+}
+
+func checkDevice(t *testing.T, config lib.Config, deviceLocalId string, deviceTypeId string) {
+	client := http.Client{
+		Timeout: 5 * time.Second,
+	}
+	req, err := http.NewRequest("GET", config.DeviceManagerUrl+"/local-devices/"+url.PathEscape(deviceLocalId), nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+	req.WithContext(ctx)
+	req.Header.Set("Authorization", string(helper.AdminJwt))
+	req.Header.Set("Accept", "application/json")
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode >= 300 {
+		responseBody, _ := ioutil.ReadAll(resp.Body)
+		err = errors.New(resp.Status + ": " + string(responseBody))
+		t.Fatal(err)
+	}
+	result := model.Device{}
+	err = json.NewDecoder(resp.Body).Decode(&result)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if result.LocalId != deviceLocalId {
+		t.Fatal("unexpected result", result)
+	}
+	if result.DeviceTypeId != deviceTypeId {
+		t.Fatal("unexpected result", result)
+	}
+}
--- test/main_test.go
+++ test/main_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"github.com/SENERGY-Platform/mqtt-platform-connector/lib"
 	"github.com/SENERGY-Platform/mqtt-platform-connector/test/server"
+	"github.com/SENERGY-Platform/platform-connector-lib/model"
 	"testing"
 	"time"
 )
@@ -30,5 +31,66 @@ func TestInit(t *testing.T) {
 		t.Error(err)
 		return
 	}
+}
 
+func TestDeviceCreation(t *testing.T) {
+	defaultConfig, err := lib.LoadConfigLocation("../config.json")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer time.Sleep(10 * time.Second) //wait for docker cleanup
+	defer cancel()
+
+	config, err := server.New(ctx, defaultConfig)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	err = lib.Start(ctx, config)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	time.Sleep(1 * time.Second)
+
+	deviceLocalId := "testservice1"
+	serviceLocalId := "testservice1"
+	deviceType := model.DeviceType{}
+	protocol := model.Protocol{}
+
+	t.Run("create protocol", func(t *testing.T) {
+		protocol = createTestProtocol(t, config)
+		time.Sleep(10 * time.Second) //wait for cqrs
+	})
+
+	t.Run("create device type", func(t *testing.T) {
+		deviceType = createTestDeviceType(t, config, protocol, serviceLocalId)
+		time.Sleep(10 * time.Second) //wait for cqrs
+	})
+
+	t.Run("send mqtt message", func(t *testing.T) {
+		sendMqttEvent(t, config, deviceType.Id+"/"+deviceLocalId+"/"+serviceLocalId, `{"level":42}`)
+		time.Sleep(10 * time.Second) //wait for cqrs
+	})
+
+	t.Run("check device creation", func(t *testing.T) {
+		checkDevice(t, config, deviceLocalId, deviceType.Id)
+	})
+}
+
+func sendMqttEvent(t *testing.T, config lib.Config, topic string, msg string) {
+	mqtt, err := lib.NewMqtt(lib.Config{AuthClientId: "sepl", AuthClientSecret: "sepl", MqttBroker: config.MqttBroker, Qos: config.Qos})
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer mqtt.Close()
+	err = mqtt.Publish(topic, msg)
+	if err != nil {
+		t.Fatal(err)
+	}
 }
